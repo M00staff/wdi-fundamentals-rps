ea75b8aa1f7550a914427f12fbca68082af4b30c
Mostafa Habib <M00staff@M00staff-MBA.fios-router.home>
2015-06-07 21:31:57 -0400

    Added Boolean OR statements

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -24,15 +24,15 @@ function randomPlay() {
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    // However, if `move` is not specified / is null, your expression should equal `getInput()`.return
+    move || `getInput()`.return;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.return 
+    move || `randomPlay()`.return;
 }
 
 function getWinner(playerMove,computerMove) {
